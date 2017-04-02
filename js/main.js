fd7fbc9b9107b7bfe4ef1059c4f250808873b3d9
SomePeaceStudio <innuss22@gmail.com>
2017-04-02 12:43:51 +0300

    simple page replacement visualization and fifo bug fixes

--- js/main.js
+++ js/main.js
@@ -111,26 +111,28 @@ function updateChart(){
     faultChart.render();
 }
 
-var BUFFER = [];
-var PAGE_FAULT = 0;
-var STEP = 0;
-
 // Run fifo algo
 function runFifo(){
     // Read input data
-        var DATA = $('#page-data-input').val().split(',').map(Number);
-        var BUFF_SIZE = parseInt($('#buffer-size-input').val());
+        var data = $('#page-data-input').val().split(',').map(Number);
+        var buffSize = parseInt($('#buffer-size-input').val());
         
         // Mesure execution time
         var FIFOstart = new Date(); 
-        fifo(DATA,BUFF_SIZE);
+        var results = fifo(data,buffSize);
         var FIFOend = new Date();
         
-        //Add data to array
-        faultData['fifo'].push(PAGE_FAULT);
+        // Return if erros where found
+        if(results == null){
+            console.log("Error in FIFO");
+            return;
+        }
+
+        // Add data to array
+        faultData['fifo'].push(results.pageFaults);
         
         // Append and display results
-        $("#results-wrap").append("<h4>FIFO : "+PAGE_FAULT+" page faults!</h4>");
+        $("#results-wrap").append("<h4>FIFO : "+results.pageFaults+" page faults!</h4>");
         $('#results-wrap').append("<h4>FIFO Time: " + (FIFOend-FIFOstart)/1000 + "s</h4>");
         $("#results-wrap").show();
         
@@ -138,69 +140,70 @@ function runFifo(){
         updateChart();
 }
 
-function fifo(data, bs){ 
-    pf = 0;
-    BUFFER = [];
-    STEP = -1;
-    PAGE_FAULT = 0;
-    var history = [];
-    var age = 0;
-    var idx; // Index for element of interest
-    for (var i = 0; i < data.length; i++){
-        STEP++;
-        renderBuffer();
-        // If page is in buffer/history
-        idx = findPage(data[i], history);
-        if(idx != -1){
-//            console.log("Element in ");
-            history[idx].age = age;
-            continue;
-        }
+// ---------- Functions used by all algorithms ----------------------------- //
 
-        // Buffer not full - add at the end
-        if(BUFFER.length<bs){
-            history.push({page:data[i],age: age})
-            updateBuffer(history);
-            age++;
-            continue;
-        }
-        
-        // If page is not in buffer/history = page fault
-        idx = findOldestIndex(history);
-        // Check for errors
-        if(idx == -1){
-            console.log("Index == -1");
+// Render buffer in current state
+// Used by every page replacement algorithm
+function renderBuffer(page, buffer, bs){
+    var table = $("#page-replace-visualizer tbody").last();
+    var i = -1; // index for buffer (escape 1.row)
+    table.children("tr").each(function(){
+        $this = $(this);
+        var content = $this.html();
+        // Add page number (1.row)
+        if(i==-1){
+            content+="<th>"+page+"</th>"
+            $this.html(content);
+            i++;
             return;
         }
-        history[idx].page = data[i];
-        history[idx].age = age;
-        PAGE_FAULT++;
-
-        // Increment "time"
-        age++;
-        updateBuffer(history);
-    }
-    renderBuffer();
-}
-
-function findOldestIndex(history){
-    var index = null;
-    var sAge = null; // smallest age
-    if(history.length<1){
-        return -1;
-    }
-    index = 0;
-    sAge = history[0].age;
-
-    for (var i = 1; i < history.length; i++){
-        if(history[i].age<sAge){
-            sAge = history[i].age;
-            index = i;
+        // Add F/H identifier (last row)
+        if(i==bs){
+            if(buffer.pageFaultIdx == -1){
+                content += "<td class=\"green\">H</td>";
+            }else{
+                content += "<td class=\"red\">F</td>";
+            }
+            $this.html(content);
+            i++;
+            return;
         }
-    }
-    return index;
+        // Add page buffer (2.row -> last row-1)
+        if(i>buffer.data.length-1){
+            content += "<td></td>";
+            $this.html(content);
+            i++;
+            return;
+        }
+        // Add red class for page fault
+        if(buffer.pageFaultIdx == i){
+            content += "<td class=\"red\">"+buffer.data[i]+"</td>";
+            $this.html(content);
+            i++;
+            return;
+        }
+        content+="<td>"+buffer.data[i]+"</td>"
+        $this.html(content);
+        i++;
+    })
 }
 
+// Initialize new render area 
+function renderBufferInit(bs){
+    // Make new table and initialize with empty rows
+    var area = $("#page-replace-visualizer");
+    area.append("<table><tbody></tbody></table>");
+    var table = $("#page-replace-visualizer tbody").last();
+    var data = "";
+    for (var i = 0; i < bs+2; i++){
+        data += "<tr></tr>";
+    }
+    table.append(data);
+}
+
+// ---------- Functions used by multiple algorithms ------------------------ //
+
+// Find page in history using page number
 function findPage(page, history){
     for (var i = 0; i < history.length; i++){
         if(history[i].page==page){
@@ -210,18 +213,107 @@ function findPage(page, history){
     return -1;
 }
 
-function updateBuffer(history){
+// Copy data from history objects to buffer array for easier rendering 
+function updateBuffer(buffer,history,pageFaultIdx){
     for (var i = 0; i < history.length; i++){
-        BUFFER[i]=history[i].page;
+        buffer.data[i]=history[i].page;
     }
-//    console.log(BUFFER);
+    buffer.pageFaultIdx = pageFaultIdx;
 }
 
-function renderBuffer(){
-    var table = $("tbody");
-    var data = "<td>STEP "+STEP+":</td>";
-    for (var i = 0; i < BUFFER.length; i++){
-        data += "<td>"+BUFFER[i]+"</td>";
+// ========================================================================= //
+// ------------------------------ FIFO ------------------------------------- //
+// ========================================================================= //
+
+// First in first out
+// int fifo(data, buffer size)
+// return: 
+//      >=0 : { page faults:int, page hits: int } 
+//      null  : error
+function fifo(data, bs){ 
+    var buffer = { 
+                    data:[], // buffer data
+                    pageFaultIdx: -1    // index where was page fault
+                                        // -1 for page hit
+                 } 
+    var pageFaults = 0;
+    var pageHits = 0;
+
+    var history = []; // page replacement history
+    var age = 0; // page place time
+    var idx; // Index for element of interest
+
+    renderBufferInit(bs);
+    for (var i = 0; i < data.length; i++){
+        // Render buffer after first cycle
+        if (i>0){
+            renderBuffer(data[i-1],buffer,bs); 
+        }
+
+        // If page is in buffer/history: page hit
+        idx = findPage(data[i], history);
+        if(idx != -1){
+            updateBuffer(buffer,history,-1);
+            pageHits++;
+            continue;
+        }
+
+        // If buffer not full: add new page
+        if(buffer.data.length<bs){
+            history.push({page:data[i],age: age})
+            updateBuffer(buffer,history,history.length-1);
+            pageFaults++;
+            age++;
+            continue;
+        }
+        
+        // If page is not in buffer: page fault
+        idx = findOldestIndex(history);
+        // If element was not found
+        if(idx == -1){
+            return null; // Error state
+        }
+        history[idx].page = data[i];
+        history[idx].age = age;
+        pageFaults++;
+        age++;
+        updateBuffer(buffer,history,idx);
     }
-    table.append("<tr>"+data+"</tr>");
+    renderBuffer(data[data.length-1],buffer,bs);
+    return {pageFaults:pageFaults,pageHits:pageHits};
 }
+
+function findOldestIndex(history){
+    var index = null;// index of oldest element
+    var sAge = null; // smallest age
+
+    // Abort if there is no elements in history
+    if(history.length<1){
+        return -1;
+    }
+
+    index = 0;
+    sAge = history[0].age;
+    for (var i = 1; i < history.length; i++){
+        if(history[i].age<sAge){
+            sAge = history[i].age;
+            index = i;
+        }
+    }
+    return index;
+}
+
+//---- FIFO END ---- //
+
+
+// ========================================================================= //
+// ------------------------------ LRU -------------------------------------- //
+// ========================================================================= //
+
+//---- LRU END ---- //
+
+// ========================================================================= //
+// -------------------------- NFU/LFU -------------------------------------- //
+// ========================================================================= //
+
+//---- NFU/LFU END ---- //
